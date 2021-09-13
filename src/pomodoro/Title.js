c5f6b71fa3cfb29c1dfe2dad472d341a7e58978b
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-13 12:30:10 -0500

    change title display if current session is break

--- src/pomodoro/Title.js
+++ src/pomodoro/Title.js
@@ -20,7 +20,11 @@ function Title({
           <div className="col">
             {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
             <h2 data-testid="session-title">
-              {session?.label} for {minutesToDuration(focusDuration)} minutes
+              {session.label} for{" "}
+              {minutesToDuration(
+                session.label === "Focusing" ? focusDuration : breakDuration
+              )}{" "}
+              minutes
             </h2>
             {/* TODO: Update message below correctly format the time remaining in the current session */}
             <p className="lead" data-testid="session-sub-title">
