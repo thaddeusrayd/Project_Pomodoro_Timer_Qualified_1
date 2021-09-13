fbce57d45924f840c8845f5c095238d90dd98ccb
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-13 13:01:36 -0500

    split into components

--- src/pomodoro/Title.js
+++ src/pomodoro/Title.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Subtitle from "./Subtitle";
+import ProgressBar from "./ProgressBar";
 
 function Title({
   session,
@@ -8,12 +10,6 @@ function Title({
   breakDuration,
 }) {
   if (session) {
-    let barPercentage;
-    session.label === "Focusing"
-      ? (barPercentage =
-          100 - (session.timeRemaining / (focusDuration * 60)) * 100)
-      : (barPercentage =
-          100 - (session.timeRemaining / (breakDuration * 60)) * 100);
     return (
       <>
         <div className="row mb-2">
@@ -26,26 +22,14 @@ function Title({
               )}{" "}
               minutes
             </h2>
-            {/* TODO: Update message below correctly format the time remaining in the current session */}
-            <p className="lead" data-testid="session-sub-title">
-              {secondsToDuration(session?.timeRemaining)} remaining
-            </p>
+            <Subtitle session={session} secondsToDuration={secondsToDuration} />
           </div>
         </div>
-        <div className="row mb-2">
-          <div className="col">
-            <div className="progress" style={{ height: "20px" }}>
-              <div
-                className="progress-bar"
-                role="progressbar"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                aria-valuenow={barPercentage} // TODO: Increase aria-valuenow as elapsed time increases
-                style={{ width: `${barPercentage}%` }} // TODO: Increase width % as elapsed time increases
-              />
-            </div>
-          </div>
-        </div>
+        <ProgressBar
+          session={session}
+          focusDuration={focusDuration}
+          breakDuration={breakDuration}
+        />
       </>
     );
   } else {
